88be23c731ec7865df5640feb01e4f455cfb97c1
Alvaro Eduardo Garzón <eduardo831_@hotmail.com>
2024-09-14 23:35:45 -0500

    feat: Added new endpoint2

--- src/IoT/crops-mock/function_app.py
+++ src/IoT/crops-mock/function_app.py
@@ -36,6 +36,25 @@ def manage_mqtt():
     client_udibot.disconnect()
 
 
+@app.route(route="hello")
+def http_trigger1_test(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        status_code=200
+    )
+
+
 @app.timer_trigger(schedule="0 */15 * * * *", arg_name="myTimer", run_on_startup=False,
                    use_monitor=False)
 def timer_trigger1(my_timer: func.TimerRequest) -> None:
