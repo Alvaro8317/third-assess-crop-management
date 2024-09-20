4657c40a583afa2565fa9d49ab44e50ac8a603b5
Alvaro Eduardo Garzón <eduardo831_@hotmail.com>
2024-09-20 12:53:46 -0500

    feat: Finished assessment

---
+++ terraform/modules/azure/output.tf
@@ -0,0 +1,3 @@
+output "URLCropManagement" {
+  value = azurerm_linux_web_app.webapp.default_hostname
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "ec2-public-ip" {
   value = module.aws.public_ip_instance
 }
+
+output "URLCropManagement" {
+  value = module.azure.URLCropManagement
+}
